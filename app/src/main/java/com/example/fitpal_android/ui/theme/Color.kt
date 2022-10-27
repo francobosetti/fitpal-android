9130b1896def0345e7bc12345ff8d6f65ffb3586
Cuinardium <sballerini@itba.edu.ar>
2022-10-27 17:49:13 -0300

    ADD: Color palette & icons

--- app/src/main/java/com/example/fitpal_android/ui/theme/Color.kt
+++ app/src/main/java/com/example/fitpal_android/ui/theme/Color.kt
@@ -6,3 +6,8 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val Orange500 = Color(0xFFFF8754) // Primary 500 Orange
+val Black000 = Color(0xFF353535) // Primary 0 Black
+val Gray400 = Color(0xFF848484) // Secondary 400 Gray this is for background
+val White100 = Color(0xFFFFFFFF) // Secondary 100 White this is for text
--- app/src/main/java/com/example/fitpal_android/ui/theme/Theme.kt
+++ app/src/main/java/com/example/fitpal_android/ui/theme/Theme.kt
@@ -3,29 +3,16 @@ package com.example.fitpal_android.ui.theme
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Orange500,
+    secondary = Black000,
+    background = Gray400,
+    onPrimary = White100,
 )
 
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
-)
+private val LightColorPalette = DarkColorPalette
 
 @Composable
 fun FitpalandroidTheme(
