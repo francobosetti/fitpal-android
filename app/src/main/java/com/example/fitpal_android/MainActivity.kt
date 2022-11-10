3da6e2d2473e91fcfb9cde6ea20b151353fc739e
Juan Cruz Bafico <86856551+jBafico@users.noreply.github.com>
2022-11-10 15:07:36 -0300

    DEV: Fixed TopBar Appearence on LogIn

--- app/src/main/java/com/example/fitpal_android/MainActivity.kt
+++ app/src/main/java/com/example/fitpal_android/MainActivity.kt
@@ -4,11 +4,14 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.material.Scaffold
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
@@ -30,34 +33,50 @@ class MainActivity : ComponentActivity() {
                 val state = rememberScaffoldState()
                 val scope = rememberCoroutineScope()
                 val navController = rememberNavController()
-
+                val topBarState= rememberSaveable{(mutableStateOf(true))}
+                val navBarState= rememberSaveable{(mutableStateOf(true))}
                 // Router current route
                 val navBackStackEntry by navController.currentBackStackEntryAsState()
+                when(navBackStackEntry?.destination?.route){
+                    Screens.LogIn.route -> {
+                        topBarState.value=false
+                        navBarState.value=false
+                    }
+                    else -> {
+                        topBarState.value =true
+                        navBarState.value= true
+                    }
+                }
 
                 Scaffold(
                     scaffoldState = state,
                     topBar = {
-                        TopBar(
-                            title = when (navBackStackEntry?.destination?.route) {
-                                Screens.Profile.route -> Screens.Profile.title
-                                Screens.Exercises.route -> Screens.Exercises.title
-                                Screens.Routines.route -> Screens.Routines.title
-                                Screens.ExploreRoutines.route -> Screens.ExploreRoutines.title
-                                Screens.DetailedExercise.route -> Screens.DetailedExercise.title
-                                Screens.DetailedRoutine.route -> Screens.DetailedRoutine.title
-                                Screens.ExecRoutine.route -> Screens.ExecRoutine.title
-                                else -> "FitPal"
-                            },
-                            imageUrl = "https://pbs.twimg.com/media/Ffn_6FDX0AAe8hk?format=jpg&name=small",
-                            onMenuClick = { scope.launch { state.drawerState.open() } },
-                            navController = navController
-                        )
+                        AnimatedVisibility(visible = topBarState.value) {
+                            TopBar(
+                                title = when (navBackStackEntry?.destination?.route) {
+                                    Screens.Profile.route -> Screens.Profile.title
+                                    Screens.Exercises.route -> Screens.Exercises.title
+                                    Screens.Routines.route -> Screens.Routines.title
+                                    Screens.ExploreRoutines.route -> Screens.ExploreRoutines.title
+                                    Screens.DetailedExercise.route -> Screens.DetailedExercise.title
+                                    Screens.DetailedRoutine.route -> Screens.DetailedRoutine.title
+                                    Screens.ExecRoutine.route -> Screens.ExecRoutine.title
+                                    else -> "FitPal"
+                                },
+                                imageUrl = "https://pbs.twimg.com/media/Ffn_6FDX0AAe8hk?format=jpg&name=small",
+                                onMenuClick = { scope.launch { state.drawerState.open() } },
+                                navController = navController
+                            )
+                        }
+
                     },
                     drawerContent = {
-                        NavigationDrawer(
-                            navController = navController,
-                            onMenuClick = { scope.launch { state.drawerState.close() } }
-                        )
+                        AnimatedVisibility(visible = navBarState.value) {
+                            NavigationDrawer(
+                                navController = navController,
+                                onMenuClick = { scope.launch { state.drawerState.close() } }
+                            )
+                        }
                     },
                 ) {
                     MyAppNavHost(
