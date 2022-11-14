0d458c9079773619173515ef4e696dc4075d0023
Gonzalo Martone <gmartone@itba.edu.ar>
2022-11-14 00:58:16 -0300

    DEV: signup now has focus + tab + enter

--- app/src/main/java/com/example/fitpal_android/ui/screens/authentication/signup/SignUp.kt
+++ app/src/main/java/com/example/fitpal_android/ui/screens/authentication/signup/SignUp.kt
@@ -1,24 +1,30 @@
 package com.example.fitpal_android.ui.screens.authentication.signup
 
+import android.view.KeyEvent
 import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.key.onKeyEvent
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
@@ -33,10 +39,13 @@ import com.example.fitpal_android.ui.theme.Gray400
 import com.example.fitpal_android.ui.theme.Orange500
 
 @Composable
-fun SignUp(onButtonClicked: () -> Unit, onLinkClicked: () -> Unit){
+fun SignUp(onButtonClicked: () -> Unit, onLinkClicked: () -> Unit) {
     val viewModel = viewModel<SignUpViewModel>()
     val signUpFormState = viewModel.signUpFormState
     val context = LocalContext.current
+    val focusManager = LocalFocusManager.current
+    val noEnterNoTabRegex = Regex("^[^\\t\\n]*\$")
+
     LaunchedEffect(key1 = context) {
         viewModel.validationEvents.collect { event ->
             when (event) {
@@ -88,7 +97,22 @@ fun SignUp(onButtonClicked: () -> Unit, onLinkClicked: () -> Unit){
                 ),
                 label = { Text(text = stringResource(R.string.profile_first_name)) },
                 value = signUpFormState.firstname,
-                onValueChange = { viewModel.onEvent(SignUpFormEvent.FirstnameChanged(it)) })
+                onValueChange = {
+                    if (it.matches(noEnterNoTabRegex)) {
+                        viewModel.onEvent(SignUpFormEvent.FirstnameChanged(it))
+                    }
+                },
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next), //TODO: elegir la accion
+                keyboardActions = KeyboardActions(
+                    onNext = { focusManager.moveFocus(FocusDirection.Next) }
+                ),
+                modifier = Modifier.onKeyEvent {
+                    if (it.nativeKeyEvent.keyCode == KeyEvent.KEYCODE_ENTER || it.nativeKeyEvent.keyCode == KeyEvent.KEYCODE_TAB) {
+                        focusManager.moveFocus(FocusDirection.Next)
+                    }
+                    false
+                }
+            )
             signUpFormState.firstnameError?.let {
                 Text(
                     text = signUpFormState.firstnameError,
@@ -111,7 +135,22 @@ fun SignUp(onButtonClicked: () -> Unit, onLinkClicked: () -> Unit){
                 ),
                 label = { Text(text = stringResource(R.string.profile_last_name)) },
                 value = signUpFormState.lastname,
-                onValueChange = { viewModel.onEvent(SignUpFormEvent.LastnameChanged(it)) })
+                onValueChange = {
+                    if (it.matches(noEnterNoTabRegex)) {
+                        viewModel.onEvent(SignUpFormEvent.LastnameChanged(it))
+                    }
+                },
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next), //TODO: elegir la accion
+                keyboardActions = KeyboardActions(
+                    onNext = { focusManager.moveFocus(FocusDirection.Next) }
+                ),
+                modifier = Modifier.onKeyEvent {
+                    if (it.nativeKeyEvent.keyCode == KeyEvent.KEYCODE_ENTER || it.nativeKeyEvent.keyCode == KeyEvent.KEYCODE_TAB) {
+                        focusManager.moveFocus(FocusDirection.Next)
+                    }
+                    false
+                }
+            )
             signUpFormState.lastnameError?.let {
                 Text(
                     text = signUpFormState.lastnameError,
@@ -134,7 +173,22 @@ fun SignUp(onButtonClicked: () -> Unit, onLinkClicked: () -> Unit){
                 ),
                 label = { Text(text = stringResource(R.string.profile_email)) },
                 value = signUpFormState.email,
-                onValueChange = { viewModel.onEvent(SignUpFormEvent.EmailChanged(it)) })
+                onValueChange = {
+                    if (it.matches(noEnterNoTabRegex)) {
+                        viewModel.onEvent(SignUpFormEvent.EmailChanged(it))
+                    }
+                },
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next), //TODO: elegir la accion
+                keyboardActions = KeyboardActions(
+                    onNext = { focusManager.moveFocus(FocusDirection.Next) }
+                ),
+                modifier = Modifier.onKeyEvent {
+                    if (it.nativeKeyEvent.keyCode == KeyEvent.KEYCODE_ENTER || it.nativeKeyEvent.keyCode == KeyEvent.KEYCODE_TAB) {
+                        focusManager.moveFocus(FocusDirection.Next)
+                    }
+                    false
+                }
+            )
 
             signUpFormState.emailError?.let {
                 Text(
@@ -161,8 +215,25 @@ fun SignUp(onButtonClicked: () -> Unit, onLinkClicked: () -> Unit){
                 label = { Text(text = stringResource(R.string.log_in_password)) },
                 value = signUpFormState.password,
                 visualTransformation = PasswordVisualTransformation(),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                onValueChange = { viewModel.onEvent(SignUpFormEvent.PasswordChanged(it)) })
+                onValueChange = {
+                    if (it.matches(noEnterNoTabRegex)) {
+                        viewModel.onEvent(SignUpFormEvent.PasswordChanged(it))
+                    }
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password,
+                    imeAction = ImeAction.Next
+                ),
+                keyboardActions = KeyboardActions(
+                    onNext = { focusManager.moveFocus(FocusDirection.Next) }
+                ),
+                modifier = Modifier.onKeyEvent {
+                    if (it.nativeKeyEvent.keyCode == KeyEvent.KEYCODE_ENTER || it.nativeKeyEvent.keyCode == KeyEvent.KEYCODE_TAB) {
+                        focusManager.moveFocus(FocusDirection.Next)
+                    }
+                    false
+                }
+            )
 
             signUpFormState.passwordError?.let {
                 Text(
@@ -174,68 +245,85 @@ fun SignUp(onButtonClicked: () -> Unit, onLinkClicked: () -> Unit){
         }
 
 
-        Spacer(modifier = Modifier.height(20.dp))
+            Spacer(modifier = Modifier.height(20.dp))
 
-        Column {
-            TextField(
-                colors = TextFieldDefaults.textFieldColors(
-                    unfocusedLabelColor = Black000,
-                    focusedIndicatorColor = Orange500,
-                    focusedLabelColor = Orange500,
-                    cursorColor = Orange500
-                ),
-                label = { Text(text = stringResource(R.string.confirm_password)) },
-                value = signUpFormState.confirmPassword,
-                visualTransformation = PasswordVisualTransformation(),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                onValueChange = { viewModel.onEvent(SignUpFormEvent.ConfirmPasswordChanged(it)) })
-
-            signUpFormState.confirmPasswordError?.let {
-                Text(
-                    text = signUpFormState.confirmPasswordError,
-                    color = MaterialTheme.colors.error,
-                    modifier = Modifier.align(Alignment.End)
-                )
-            }
-        }
-
-
-        Spacer(modifier = Modifier.height(20.dp))
-        Box(modifier = Modifier.padding(40.dp, 0.dp, 40.dp, 0.dp)) {
-            Button(
-                onClick = { viewModel.onEvent(SignUpFormEvent.SignUp) },
-                modifier = Modifier
-                    .height(50.dp)
-                    .width(140.dp),
-                shape = RoundedCornerShape(10.dp),
-                colors = ButtonDefaults.buttonColors(backgroundColor = Orange500)
-            ) {
-                Text(
-                    text = stringResource(R.string.sign_up_button),
-                    style = TextStyle(
-                        fontSize = 18.sp,
-                        textAlign = TextAlign.Center
+            Column {
+                TextField(
+                    colors = TextFieldDefaults.textFieldColors(
+                        unfocusedLabelColor = Black000,
+                        focusedIndicatorColor = Orange500,
+                        focusedLabelColor = Orange500,
+                        cursorColor = Orange500
                     ),
-                    color = Color.White
+                    label = { Text(text = stringResource(R.string.confirm_password)) },
+                    value = signUpFormState.confirmPassword,
+                    visualTransformation = PasswordVisualTransformation(),
+                    onValueChange = {
+                        if (it.matches(noEnterNoTabRegex)) {
+                            viewModel.onEvent(SignUpFormEvent.ConfirmPasswordChanged(it))
+                        }
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Password,
+                        imeAction = ImeAction.Next
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onNext = { focusManager.clearFocus() }
+                    ),
+                    modifier = Modifier.onKeyEvent {
+                        if (it.nativeKeyEvent.keyCode == KeyEvent.KEYCODE_ENTER || it.nativeKeyEvent.keyCode == KeyEvent.KEYCODE_TAB) {
+                            focusManager.clearFocus()
+                        }
+                        false
+                    }
+                )
+
+                signUpFormState.confirmPasswordError?.let {
+                    Text(
+                        text = signUpFormState.confirmPasswordError,
+                        color = MaterialTheme.colors.error,
+                        modifier = Modifier.align(Alignment.End)
+                    )
+                }
+            }
+
+
+            Spacer(modifier = Modifier.height(20.dp))
+            Box(modifier = Modifier.padding(40.dp, 0.dp, 40.dp, 0.dp)) {
+                Button(
+                    onClick = { viewModel.onEvent(SignUpFormEvent.SignUp) },
+                    modifier = Modifier
+                        .height(50.dp)
+                        .width(140.dp),
+                    shape = RoundedCornerShape(10.dp),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Orange500)
+                ) {
+                    Text(
+                        text = stringResource(R.string.sign_up_button),
+                        style = TextStyle(
+                            fontSize = 18.sp,
+                            textAlign = TextAlign.Center
+                        ),
+                        color = Color.White
+                    )
+                }
+            }
+            Box(modifier = Modifier.fillMaxSize()) {
+                ClickableText(
+                    text = AnnotatedString(stringResource(R.string.log_in_link)),
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .padding(20.dp),
+                    onClick = { onLinkClicked() },
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        fontFamily = FontFamily.Default,
+                        textDecoration = TextDecoration.Underline,
+                        color = Orange500
+                    )
                 )
             }
-        }
-        Box(modifier = Modifier.fillMaxSize()) {
-            ClickableText(
-                text = AnnotatedString(stringResource(R.string.log_in_link)),
-                modifier = Modifier
-                    .align(Alignment.BottomCenter)
-                    .padding(20.dp),
-                onClick = { onLinkClicked() },
-                style = TextStyle(
-                    fontSize = 14.sp,
-                    fontFamily = FontFamily.Default,
-                    textDecoration = TextDecoration.Underline,
-                    color = Orange500
-                )
-            )
-        }
 
 
-    }
+        }
 }
