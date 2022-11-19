dbca3a2c9a389143c13665fdfb3a650f0e4b6786
Gonzalo Martone <gmartone@itba.edu.ar>
2022-11-18 23:16:42 -0300

    Better feedback

--- app/src/main/java/com/example/fitpal_android/ui/screens/appContent/detailedRoutine/DetailedRoutine.kt
+++ app/src/main/java/com/example/fitpal_android/ui/screens/appContent/detailedRoutine/DetailedRoutine.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.fitpal_android.ui.components.cards.detailed.DetailedRoutineCard
 import com.example.fitpal_android.ui.theme.Orange500
@@ -81,8 +82,19 @@ fun DetailedRoutine(
                 }
             } else {
                 if (routineId != null && detailedRoutineState.routine != null) {
-                    detailedRoutineState.routine
 
+                    if(detailedRoutineState.loadingInput) {
+                        Dialog(
+                            onDismissRequest = { /*TODO*/ }
+                        ) {
+                            CircularProgressIndicator(
+                                color = Orange500,
+                                strokeWidth = 4.dp,
+                                modifier = Modifier
+                                    .padding(10.dp)
+                            )
+                        }
+                    }
                     // Routine details
                     DetailedRoutineCard(
                         name = detailedRoutineState.routine.name,
--- app/src/main/java/com/example/fitpal_android/ui/screens/appContent/detailedRoutine/DetailedRoutineViewModel.kt
+++ app/src/main/java/com/example/fitpal_android/ui/screens/appContent/detailedRoutine/DetailedRoutineViewModel.kt
@@ -32,29 +32,28 @@ class DetailedRoutineViewModel(
         viewModelScope.launch {
             detailedRoutineState = detailedRoutineState.copy(isFetching = true, error = "")
 
-            try {
+            detailedRoutineState = try {
                 val routine = routineRepository.getRoutine(routineId)
                 val rating = routineRepository.getRoutineUserScore(routineId)
 
-                detailedRoutineState = detailedRoutineState.copy(
+                detailedRoutineState.copy(
                     routine = routine,
                     userRating = rating,
-                    isFetching = false,
                     error = ""
                 )
             } catch (e: Exception) {
-                detailedRoutineState = detailedRoutineState.copy(
-                    isFetching = false,
+                detailedRoutineState.copy(
                     error = e.message ?: "Unknown error"
                 )
             }
 
+            detailedRoutineState = detailedRoutineState.copy(isFetching = false)
         }
     }
 
     fun rateRoutine(rating: Double) {
         viewModelScope.launch {
-            detailedRoutineState = detailedRoutineState.copy(isFetching = true, error = "")
+            detailedRoutineState = detailedRoutineState.copy(loadingInput = true, error = "")
 
             try {
                 routineRepository.rateRoutine(routineId, rating)
@@ -64,7 +63,6 @@ class DetailedRoutineViewModel(
 
                 detailedRoutineState = detailedRoutineState.copy(
                     routine = routine,
-                    isFetching = false,
                     error = ""
                 )
 
@@ -72,10 +70,10 @@ class DetailedRoutineViewModel(
 
             } catch (e: Exception) {
                 detailedRoutineState = detailedRoutineState.copy(
-                    isFetching = false,
                     error = e.message ?: "Unknown error"
                 )
             }
+            detailedRoutineState = detailedRoutineState.copy(loadingInput = false)
         }
         // reset rating
         detailedRoutineState = detailedRoutineState.copy(reviewRating = 0.0)
@@ -83,7 +81,7 @@ class DetailedRoutineViewModel(
 
     fun toggleFav() {
         viewModelScope.launch {
-            detailedRoutineState = detailedRoutineState.copy(isFetching = true, error = "")
+            detailedRoutineState = detailedRoutineState.copy(loadingInput = true, error = "")
 
             try {
                 if (detailedRoutineState.routine?.isFavorite == true) {
@@ -97,17 +95,16 @@ class DetailedRoutineViewModel(
 
                 detailedRoutineState = detailedRoutineState.copy(
                     routine = routine,
-                    isFetching = false,
                     error = ""
                 )
 
                 validationEventChannel.send(DetailedRoutineEvent.FavoriteToggled)
             } catch (e: Exception) {
                 detailedRoutineState = detailedRoutineState.copy(
-                    isFetching = false,
                     error = e.message ?: "Unknown error"
                 )
             }
+            detailedRoutineState = detailedRoutineState.copy(loadingInput = false)
         }
     }
 
